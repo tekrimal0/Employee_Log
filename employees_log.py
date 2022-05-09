c0a0c07c3dfe6eddd2bf8b9e14af9d00f9167d6e
tekrimal0 <103054022+tekrimal0@users.noreply.github.com>
2022-05-09 09:09:56 -0500

    Add files via upload

--- employees_log.py
+++ employees_log.py
@@ -5,7 +5,39 @@ import pandas as pa
 #connecting to mongoDB
 client = pymongo.MongoClient('mongodb://localhost:27017/')
 db = client.test
+logins = db.logins
 employees = db.employees
+
+#function to create new login
+def create_login():
+    new_user = input("Please create new username: ")
+    login_availability = db.logins.count_documents({"Username" : new_user})
+    if login_availability == 1:
+        print("Username already exist, please try creating with different username.\n")
+        create_login()
+    else:
+        new_password = input("Please create new password: ")
+        new_login = db.logins.insert_one({"Username": new_user, "Password": new_password})
+        print("Login created successfully.\n")
+        print(new_login, "\n")
+        login_menu()
+
+#function to log into the dashbord
+def existing_login():
+    user_in = input("Please enter your username: ")
+    user_existence = db.logins.count_documents({"Username": user_in})
+    if user_existence == 0:
+        print("Username doesn't exist, please try again.\n")
+        existing_login()
+    else:
+        password_in = input("Please enter your password: ")
+        password_existence = db.logins.count_documents({"Password": password_in})
+        if password_existence == 0:
+            print("Incorrect password, please try logging again.\n")
+            existing_login()
+        else:
+            print("Login successful.\n")
+            employees_log()
 
 #function to get the list of the documents present in mongoDB collection
 def viewingall_employees():
@@ -51,29 +83,82 @@ def adding_employees():
         viewingall_employees()
         employees_log()
 
-#function to update existing document in the collection
-def updating_employees():
+#functions to update one particular document at a time
+def update_fullname():
     ID_upd = int(input("Please enter ID of an employee who need update: "))
     data_existence = db.employees.count_documents({"ID" : ID_upd})
     if data_existence == 0:
         print("Employee with this ID doesn't exist, please try again with different ID.\n")
-        employees_log()
+        update_menu()
     else:
-        PhoneNumber_upd = input("Please enter employee's phone number: ")
-        Department_upd = input("Please enter employee's department: ")
-        Payrate_upd = int(input("Please enter employee's pay-rate per hour: "))
+        FullName_upd = input("Please enter employee's updated full name: ")
         update = db.employees.update_one(
             {"ID" : ID_upd},
             {"$set" :
-                {"PhoneNumber" : PhoneNumber_upd,
-                "Department" : Department_upd,
-                "Payrate" : Payrate_upd
+                {"Name" : FullName_upd
                 }}
         )
-        print("Updating employee completed.")
+        print("Updating employee's name completed.")
         print(update, "\n")
         viewingall_employees()
-        employees_log()
+        update_menu()
+
+def update_phonenumber():
+    ID_upd = int(input("Please enter ID of an employee who need update: "))
+    data_existence = db.employees.count_documents({"ID" : ID_upd})
+    if data_existence == 0:
+        print("Employee with this ID doesn't exist, please try again with different ID.\n")
+        update_menu()
+    else:
+        PhoneNumber_upd = input("Please enter employee's updated phone number: ")
+        update = db.employees.update_one(
+            {"ID" : ID_upd},
+            {"$set" :
+                 {"PhoneNumber" : PhoneNumber_upd
+                  }}
+        )
+        print("Updating employee's phone number completed.")
+        print(update, "\n")
+        viewingall_employees()
+        update_menu()
+
+def update_department():
+    ID_upd = int(input("Please enter ID of an employee who need update: "))
+    data_existence = db.employees.count_documents({"ID" : ID_upd})
+    if data_existence == 0:
+        print("Employee with this ID doesn't exist, please try again with different ID.\n")
+        update_menu()
+    else:
+        Department_upd = input("Please enter employee's updated department: ")
+        update = db.employees.update_one(
+            {"ID" : ID_upd},
+            {"$set" :
+                 {"Department" : Department_upd
+                  }}
+        )
+        print("Updating employee's department completed.")
+        print(update, "\n")
+        viewingall_employees()
+        update_menu()
+
+def update_payrate():
+    ID_upd = int(input("Please enter ID of an employee who need update: "))
+    data_existence = db.employees.count_documents({"ID" : ID_upd})
+    if data_existence == 0:
+        print("Employee with this ID doesn't exist, please try again with different ID.\n")
+        update_menu()
+    else:
+        Payrate_upd = int(input("Please enter employee's updated payrate: "))
+        update = db.employees.update_one(
+            {"ID" : ID_upd},
+            {"$set" :
+                 {"Payrate" : Payrate_upd
+                  }}
+        )
+        print("Updating employee's payrate completed.")
+        print(update, "\n")
+        viewingall_employees()
+        update_menu()
 
 #funtion to delete existing document from the collection
 def deleting_employees():
@@ -89,14 +174,38 @@ def deleting_employees():
         viewingall_employees()
         employees_log()
 
+#function to create menu for update
+def update_menu():
+    print("Please enter 1 to update FullName.")
+    print("Please enter 2 to update PhoneNumber.")
+    print("Please enter 3 to update Department.")
+    print("Please enter 4 to update Payrate.")
+    print("Please enter 5 to go back to the dashboard.\n")
+
+    update_choice = int(input("Please choose your entry: "))
+    if update_choice == 1:
+        update_fullname()
+    elif update_choice == 2:
+        update_phonenumber()
+    elif update_choice == 3:
+        update_department()
+    elif update_choice == 4:
+        update_payrate()
+    elif update_choice == 5:
+        employees_log()
+    else:
+        print("Invalid entry, please try again.\n")
+        update_menu()
+
 #function to create menu to operate CRUD in collection
 def employees_log():
-    print("Welcome to the Employees Detail Log")
+    print("Welcome to the EmployeesLog Dashboard!")
     print("Please enter 1 to view an existing employee.")
     print("Please enter 2 to add new employee.")
     print("Please enter 3 to update an existing employee.")
     print("Please enter 4 to delete an existing employee.")
-    print("Please enter 5 to exit out of it.\n")
+    print("Please enter 5 to log out and go back to login page.")
+    print("Please enter 6 to exit out of it.\n")
 
     choice = int(input("Please choose your entry: "))
     if choice == 1:
@@ -104,15 +213,34 @@ def employees_log():
     elif choice == 2:
         adding_employees()
     elif choice == 3:
-        updating_employees()
+        update_menu()
     elif choice == 4:
         deleting_employees()
     elif choice == 5:
+        login_menu()
+    elif choice == 6:
         exit()
     else:
-        print("Invalid entry, please try again.")
+        print("Invalid entry, please try again.\n")
         employees_log()
-employees_log()
 
+#function to create menu for login page
+def login_menu():
+    print("Welcome to login page for EmployeeLog ")
+    print("Please enter 1 to login into the EmployeeLog.")
+    print("Please enter 2 to create login credentials.")
+    print("Please enter 3 to exit out of it.\n")
+
+    login_choice = int(input("Please choose your entry: "))
+    if login_choice == 1:
+        existing_login()
+    elif login_choice == 2:
+        create_login()
+    elif login_choice == 3:
+        exit()
+    else:
+        print("Invalid entry, please try again!")
+        login_menu()
+login_menu()
 
 
